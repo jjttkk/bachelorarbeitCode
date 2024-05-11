628cb938af2c784c7afa8dfd58cfb259b871fda7
Jamie <uvlkb@student.kit.edu>
2024-05-11 22:56:53 +0200

    enc und dec mit map weil monade mmn unnötig und ansatz für dec(enc(x)=x beweise

--- verifyEncryptionDecryption.thy
+++ verifyEncryptionDecryption.thy
@@ -1,56 +1,77 @@
-theory verifyPlurality imports
+theory verifyEncryptionDecryption imports
 "Game_Based_Crypto.Elgamal"
 begin
 
 context elgamal_base
 begin
 
-fun ballotEncrypt :: "'grp pub_key \<Rightarrow> 'grp list \<Rightarrow> 'grp cipher spmf list" 
-  where
-    "ballotEncrypt pk [] = []" |
-    "ballotEncrypt pk (x # xs) = (aencrypt pk x) # ballotEncrypt pk xs"
+(*Function to encrypt a list of elements using Elgamal-Encryption from Game Based Crypto*)
+fun enc_list:: "'grp pub_key \<Rightarrow> 'grp list \<Rightarrow> 'grp cipher spmf list"
+  where "enc_list \<alpha> ls = (map (aencrypt \<alpha>) ls)"
 
-definition ballot_aencrypt :: "'grp pub_key \<Rightarrow> 'grp list \<Rightarrow> 'grp cipher spmf list"
+(*Function to decrypt a list of elements using Elgamal-Decryption from Game Based Crypto*)
+fun dec_list :: "'grp priv_key \<Rightarrow> 'grp cipher spmf list \<Rightarrow> 'grp option spmf list" 
 where
-  "ballot_aencrypt pk (x # xs) = do {
-   return_spmf (aencrypt pk x # ballot_aencrypt pk xs)
-  }"
+ "dec_list \<beta> ciphers = map (\<lambda>c. map_spmf (adecrypt \<beta>) c) ciphers"
 
-definition ballot_adecrypt :: "'grp priv_key \<Rightarrow> 'grp cipher spmf list \<Rightarrow> 'grp option list"
-where
-  "ballot_adecrypt x = (\<lambda>(\<beta>, \<zeta>). Some (\<zeta> \<otimes> (inv (\<beta> [^] x))))"
+(*Function to parse the type for proving*)
+fun fix_type:: "'grp list \<Rightarrow> 'grp option spmf list"
+  where "fix_type xs = map (\<lambda>x. return_spmf (Some x)) xs" 
 
-(*fun ballotDecrypt :: "'grp priv_key \<Rightarrow> 'grp cipher spmf list \<Rightarrow> 'grp option list"
-  where 
-    "ballotDecrypt sk xss = xss \<bind> (\<lambda>xs. map (\<lambda>y. adecrypt sk y))"*) (* do {
-      xs \<leftarrow> xss
-      map (\<lambda>x. adecrypt sk x) xs
-    }"*)
-    
-fun ballotDecrypt :: "'grp priv_key \<Rightarrow> 'grp cipher list \<Rightarrow> 'grp option list"
-  where 
-    ballotDecryptNil: "ballotDecrypt sk [] = []" |
-    ballotDecryptCons: "ballotDecrypt sk (x # xs) = (adecrypt sk x) # ballotDecrypt sk xs"
+
+(*x option spmf = dec (enc x)*)
+
+lemma encrypt_then_decrypt_same_message: 
+  assumes "msg \<in> carrier \<G>" 
+  shows "adecrypt x (aencrypt \<alpha> msg) = Some msg"
+proof -
+  have "adecrypt x (aencrypt \<alpha> msg) = adecrypt x (\<^bold>g [^] y, (\<alpha> [^] y) \<otimes> msg)"
+    unfolding aencrypt_def adecrypt_def by simp
+  also have "... = Some (((\<alpha> [^] y) \<otimes> msg) \<otimes> (inv (\<^bold>g [^] y [^] x)))"
+    using assms unfolding aencrypt_def adecrypt_def by simp
+  also have "... = Some (((\<alpha> [^] y) \<otimes> msg) \<otimes> (\<^bold>g [^] (y * x)))"
+    using assms by (simp add: nat_pow_mult)
+  also have "... = Some (msg \<otimes> (\<^bold>g [^] (y * x)))"
+    using assms by (simp add: m_assoc)
+  also have "... = Some msg" 
+    using assms by (simp add: nat_pow_pow)
+  finally show ?thesis .
+qed
+
+definition \<alpha> :: "'grp pub_key" where "\<alpha> = g [^] x"
+ 
+lemma aencrypt_adecrypt:
+  "adecrypt x (aencrypt \<alpha> msg) = Some msg"
+proof -
+  have "adecrypt x (aencrypt \<alpha> msg) = Some (snd (aencrypt \<alpha> msg) \<otimes> inv (fst (aencrypt \<alpha> msg) [^] x))"
+    by (simp add: aencrypt_def adecrypt_def)
+  also have "... = Some (((\<alpha> [^] y) \<otimes> msg) \<otimes> inv ((g [^] y) [^] x))"
+    by (simp add: \<alpha>_def)
+  also have "... = Some (g [^] (x * y) \<otimes> msg \<otimes> inv (g [^] (y * x)))"
+    by (simp add: pow_mult_distrib)
+  also have "... = Some (g [^] 0 \<otimes> msg)"
+    by (simp add: pow_mult_inverse)
+  also have "... = Some msg"
+    by (simp add: pow_zero_ident mult_assoc)
+  finally show ?thesis .
+qed
+
+(*lemma dec_enc: "adecrypt sk (aencrypt pk y) = y"
+
+theorem ballot_enc_dec: "ballotEncrypt pk (ballotDecrypt sk xs) = xs"
+  apply (induct xs)
+  apply auto
+
+lemma dec_enc:
+    "ballotDecrypt sk (ballotEncrypt pk ys) = ys"
 
 lemma enc_dec: "aencrypt pk (adecrypt sk x) = x"
 
 lemma dec_enc: "adecrypt sk (aencrypt pk y) = y"
 
 theorem ballot_enc_dec: "ballotEncrypt pk (ballotDecrypt sk xs) = xs"
-  apply (induct xs)
-  apply auto
 
-(*lemma dec_enc:
-    "ballotDecrypt sk (ballotEncrypt pk ys) = ys"*)
-
-(*lemma enc_dec: "aencrypt pk (adecrypt sk x) = x"
-
-lemma dec_enc: "adecrypt sk (aencrypt pk y) = y"*)
-
-theorem ballot_enc_dec: "ballotEncrypt pk (ballotDecrypt sk xs) = xs"
-
-
-(*lemma dec_enc:
+lemma dec_enc:
     "ballotDecrypt sk (ballotEncrypt pk ys) = ys"*)
 
 end
--- verifyPlurality.thy
+++ verifyPlurality.thy
@@ -7,19 +7,7 @@ context elgamal_base
 begin
 
 datatype ('a, 'b) tupel_list = Nil | Cons "('a \<times> 'b)" "('a, 'b) tupel_list"
-(*
-definition one :: "nat" where "one =  1"
-definition zeros :: "nat list" where "zeros = replicate 0 (0::nat)"
-definition plurality_values :: "nat list" where "plurality_values = one # zeros"
-*)
 
-(*nat list is list of preferences, [1,0,0,...] for Plurality-Voting
-  Preference_List already encrypted*)(*
-fun convert_preferencelist_to_ballot :: "'a Preference_List \<Rightarrow> nat list \<Rightarrow> ('a, 'b)  ballot"
-  where
-   "convert_preferencelist_to_ballot [] _ = Nil" |
-   "convert_preferencelist_to_ballot (x # xs) (y # ys) = Cons(x, aencrypt (y)) (convert_preferencelist_to_ballot xs ys) "
-*)
 (*first ballot is new ballot to add,
  second ballot is Sum-Ballot which holds final result at the end*)(*
 fun add_ballots :: "('a, 'b) ballot \<Rightarrow>('a, 'b) ballot \<Rightarrow>('a, 'b) ballot "
@@ -31,16 +19,16 @@ fun add_ballots :: "('a, 'b) ballot \<Rightarrow>('a, 'b) ballot \<Rightarrow>('
     Some (option_sum, value_sum) \<Rightarrow> (option_sum,value_sum + value_b) # add ballots options sum_ballot"
       *)
 
-(*The Preference_List is already encrypted, this function takes the encrypted Preference_List
- finds the top-preference/first element of the Preference-List in the tupel_list representing 
- the Ballot where the votes are accumulated and adds 1 
+(*The Preference_List is already encrypted, 
+ this function takes the encrypted Preference_List finds its first element in
+ the tupel_list representing the Ballot where the votes are accumulated and adds 1 
  (encrypted 1 because of the homomorphic adding)*)
-fun add_plurality_ballot :: "'a Preference_List \<Rightarrow> ('a, 'b) tupel_list \<Rightarrow> ('a, 'b) tupel_list"
+fun add_plurality_ballot :: "'grp pub_key \<Rightarrow>'a Preference_List \<Rightarrow> ('a \<times> 'b) list \<Rightarrow> ('a \<times> 'b) list"
     where
-    "add_plurality_ballot (sum_list::('a,'b) tupel_list) = sum_list"|
-    "add_plurality_ballot (x # xs) (sum_list::('a,'b) tupel_list) = 
-    (case find (\<lambda>(y,_). x = y) sum_list of
-      None \<Rightarrow> add_plurality_ballot xs(sum_list::('a,'b) tupel_list) | 
-      Some (y, count) \<Rightarrow> Cons (y,count + aencrypt(pk, 1)) # remove1 (\<lambda>(a,_).a = y) sum_list)"
+    "add_plurality_ballot _ [] (s) = s"|
+    "add_plurality_ballot pk (x # xs) (s) = 
+    (case find (\<lambda>(y,c). (x = y)) s of
+      None \<Rightarrow> add_plurality_ballot pk xs (s) | 
+      Some (y, c) \<Rightarrow> (y,c + (aencrypt pk 1)) # remove1 (\<lambda>(a,_).a = y) s)"
 end
 end
