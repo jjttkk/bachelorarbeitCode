23a975e5547e5471448dff232e7ae4f328255666
Jamie <uvlkb@student.kit.edu>
2024-05-13 16:19:52 +0200

    add_plurality_ballot funktioniert

--- verifyPlurality.thy
+++ verifyPlurality.thy
@@ -4,31 +4,31 @@ theory verifyPlurality imports
 "verifiedVotingRuleConstruction/theories/Compositional_Structures/Basic_Modules/Component_Types/Social_Choice_Types/Refined_Types/Preference_List"
 begin
 context elgamal_base
-begin
+begin 
 
-datatype ('a, 'b) tupel_list = Nil | Cons "('a \<times> 'b)" "('a, 'b) tupel_list"
+fun get_start_s :: "'grp pub_key \<Rightarrow> 'grp cipher spmf list  \<Rightarrow> ('grp cipher spmf \<times> 'grp cipher spmf) list" where
+"get_start_s pk [] = []"|
+"get_start_s pk (x # xs) = (x, (aencrypt pk (one \<G>))) # get_start_s pk xs"
 
-(*first ballot is new ballot to add,
- second ballot is Sum-Ballot which holds final result at the end*)(*
-fun add_ballots :: "('a, 'b) ballot \<Rightarrow>('a, 'b) ballot \<Rightarrow>('a, 'b) ballot "
-  where
-  "add_ballots [] sum_ballot = sum_ballot"|
-  "add_ballots ((option_sum, value_b) # options) sum_ballot = 
-  (case find (\<lambda>(option_sum, value_sum). option_b = option_sum) sum_ballot of
-    None \<Rightarrow> add_ballots options sum_ballot |
-    Some (option_sum, value_sum) \<Rightarrow> (option_sum,value_sum + value_b) # add ballots options sum_ballot"
-      *)
+definition add_pair :: "('grp \<times> 'grp) spmf \<Rightarrow> ('grp \<times> 'grp) spmf \<Rightarrow> ('grp \<times> 'grp) spmf" where
+"add_pair x_spmf y_spmf = do {
+    (x1, x2) \<leftarrow> x_spmf;
+    (y1, y2) \<leftarrow> y_spmf;
+    return_spmf (x1, x2 \<otimes> y2)
+}"
 
-(*The Preference_List is already encrypted, 
+
+(*The Preference_List is already encrypted,
  this function takes the encrypted Preference_List finds its first element in
- the tupel_list representing the Ballot where the votes are accumulated and adds 1 
+ the Pair-List representing the Ballot where the votes are accumulated and adds 1 
  (encrypted 1 because of the homomorphic adding)*)
-fun add_plurality_ballot :: "'grp pub_key \<Rightarrow>'a Preference_List \<Rightarrow> ('a \<times> 'b) list \<Rightarrow> ('a \<times> 'b) list"
+fun add_plurality_ballot :: "'grp pub_key \<Rightarrow>'grp cipher spmf Preference_List \<Rightarrow> ('grp cipher spmf \<times> 'grp cipher spmf) list \<Rightarrow> ('grp cipher spmf \<times> 'grp cipher spmf) list"
     where
-    "add_plurality_ballot _ [] (s) = s"|
-    "add_plurality_ballot pk (x # xs) (s) = 
+    "add_plurality_ballot _ [] s = s"|
+    "add_plurality_ballot pk (x # xs) s = 
     (case find (\<lambda>(y,c). (x = y)) s of
-      None \<Rightarrow> add_plurality_ballot pk xs (s) | 
-      Some (y, c) \<Rightarrow> (y,c + (aencrypt pk 1)) # remove1 (\<lambda>(a,_).a = y) s)"
+      None \<Rightarrow> add_plurality_ballot pk xs s | 
+      Some (y, c) \<Rightarrow> let new_s = remove1 (y, c) s in
+                 (y, add_pair(c::'grp cipher spmf) (aencrypt pk (one \<G>))) # new_s)"
 end
 end
